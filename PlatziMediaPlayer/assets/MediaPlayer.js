dec493ea78ae10908a0dfc3646b46fe2362adaa7
israellp24 <41647280+israellp24@users.noreply.github.com>
2022-04-18 23:24:01 -0500

    fin clase 18

--- PlatziMediaPlayer/assets/MediaPlayer.js
+++ PlatziMediaPlayer/assets/MediaPlayer.js
@@ -6,6 +6,22 @@ function MediaPlayer(config) {
 }
 
 MediaPlayer.prototype._initPlugins = function () {
+  const player = {
+    play: ()=> this.play(),
+    pause: () => this.pause(),
+    media: this.media,
+    get muted (){
+      return this.media.muted;
+    },
+    set muted(value){
+      if (value==true) {
+        this.media.muted = true;
+      }else{
+        this.media.muted = false;
+      }
+    }
+  }
+
   this.plugins.forEach(plugin => {
     plugin.run(this);
   });
