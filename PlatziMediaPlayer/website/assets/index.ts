32b78564ef7871a23fe0c8316b46f1b316d73313
israellp24 <41647280+israellp24@users.noreply.github.com>
2022-05-09 23:56:08 -0500

    npm init

--- PlatziMediaPlayer/website/assets/index.ts
+++ PlatziMediaPlayer/website/assets/index.ts
@@ -1,13 +1,15 @@
-import MediaPlayer from "./MediaPlayer";
-import AutoPlay from "./plugins/AutoPlay";
-import AutoPause from "./plugins/AutoPause";
+import MediaPlayer from "@israellp24/platzimediaplayer";
+import AutoPlay from "@israellp24/platzimediaplayer/src/plugins/AutoPlay";
+import AutoPause from "@israellp24/platzimediaplayer/src/plugins/AutoPause";
+import Ads from "@israellp24/platzimediaplayer/src/plugins/Ads";
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({ 
     el: video,
     plugins: [
         new AutoPlay(),
-        new AutoPause()
+        new AutoPause(),
+        new Ads()
     ] 
 });
 
